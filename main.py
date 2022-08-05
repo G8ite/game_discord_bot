c445c587b938ae11cba38345d9ea481af8b4316a
Amandine Pimentel de Andrade <70423300+G8ite@users.noreply.github.com>
2022-08-05 19:17:29 +0200

    gestion du token

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import discord
+import os
 from discord.ext import commands
-from Data.database_handler import DatabaseHandler
-from gif_game_cog import Gif_game_cog 
+from gif_game_cog import Gif_game_cog
+from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix="/", description="bot de test")
-database_handler = DatabaseHandler("Data/discord-bot.db")
+load_dotenv(dotenv_path="config")
 
 @bot.event
 async def on_ready():
@@ -12,4 +12,4 @@ async def on_ready():
 
 bot.add_cog(Gif_game_cog(bot))
 
-bot.run("MTAwNTA5OTM3MTYxMzkxMzEwOA.GxPQbS.EQjiHgdHqM9twrCPYZDptUSf05zLmcQmizowlY")
+bot.run(os.getenv("TOKEN"))
